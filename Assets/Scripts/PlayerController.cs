f7b1012b3fec37db53a3278262550666e5692312
Sehyun Kim <mathshyun@gmail.com>
2025-04-05 16:51:20 +0900

    Add obstacle and player camera control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,40 +2,62 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float accelerationRate;
-    public float maxSpeed;
-    public float rotationRate;
-
+    public float accelerationFactor;
+    public float rotationFactor;
+    public float horizontalForceFactor;
+    public float maxSpeedFactor;
+    
     [SerializeField] private Transform cameraTransform;
-    [SerializeField] private Vector3 cameraPositionOffset;
-
+    [SerializeField] private Vector3[] cameraOffsets;
+    [SerializeField] private Vector3 cameraOffset;
+    
     private Rigidbody _rigidbody;
-
+    
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.centerOfMass = new Vector3(0f, -0.5f, -0.2f);
+        cameraOffset = cameraOffsets[0];
     }
     
-    private void Update()
+    private void FixedUpdate()
     {
         var verticalInput = Input.GetAxis("Vertical");
         var horizontalInput = Input.GetAxis("Horizontal");
         var forwardVelocity = Vector3.Dot(_rigidbody.velocity, transform.forward);
         
         // Accelerate
-        _rigidbody.AddForce(verticalInput * accelerationRate * Time.deltaTime * transform.forward);
+        var accelerationRate =
+            Mathf.Sqrt(Mathf.Clamp01((maxSpeedFactor - (verticalInput >= 0 ? forwardVelocity : -forwardVelocity)) /
+                                     maxSpeedFactor));
+        _rigidbody.AddForce(verticalInput * accelerationRate * accelerationFactor * Time.fixedDeltaTime *
+                            transform.forward);
         
         // Rotate
-        _rigidbody.angularVelocity = horizontalInput * rotationRate * forwardVelocity * transform.up;
-        _rigidbody.AddForce(horizontalInput * rotationRate * forwardVelocity * Time.deltaTime * transform.right);
+        _rigidbody.angularVelocity = horizontalInput * forwardVelocity * rotationFactor * transform.up;
+        _rigidbody.AddForce(horizontalInput * forwardVelocity * horizontalForceFactor * Time.fixedDeltaTime *
+                            transform.right);
         
-        Debug.Log($"forwardVelocity: {forwardVelocity}, " +
-                  $"angularVelocity: {_rigidbody.angularVelocity}");
+        // Debug.Log($"forwardVelocity: {forwardVelocity}, " +
+        //           $"angularVelocity: {_rigidbody.angularVelocity}");
     }
-
+    
     private void LateUpdate()
     {
-        cameraTransform.position = transform.TransformPoint(cameraPositionOffset);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            cameraOffset = cameraOffsets[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            cameraOffset = cameraOffsets[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            cameraOffset = cameraOffsets[2];
+        }
+        
+        cameraTransform.position = transform.TransformPoint(cameraOffset);
         cameraTransform.LookAt(transform);
     }
 }
